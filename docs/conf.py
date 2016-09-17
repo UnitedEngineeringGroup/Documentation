11851e04c2e287b193c07efa6f804e9192f49367
jakingtosch <jhmaretzki@gmail.com>
2016-09-17 21:47:52 +0200

    changed theme to default

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,8 @@
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
+import os
+
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -120,6 +122,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
